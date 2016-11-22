54801948b0653b1e7766a698581aaf6fdc0c01b1
Juanlu <jurastrol@alumnos.unex.es>
2016-11-22 17:33:14 +0100

    asdfasd

--- src/CMakeFiles/Security_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Security_automoc.cpp"
-  "CMakeFiles/Security_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Security_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
